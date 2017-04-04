763e750a90d7bb88405c3590f97ace3091081035
Zachary Smith <OothecaPickleGNUrmsTUXFSF@users.noreply.github.com>
2017-04-03 18:51:32 -0700

    Add Dependencies

--- idevicerestore.rb
+++ idevicerestore.rb
@@ -3,7 +3,17 @@ require 'formula'
 class Idevicerestore < Formula
   head 'https://github.com/libimobiledevice/idevicerestore.git'
 
-  # depends_on 'make' => :build
+  depends_on 'make' => :build
+  depends_on 'libimobiledevice' => :build
+  depends_on 'usbmuxd' => :build
+  depends_on 'libplist' => :build
+  depends_on 'libzip' => :build
+  depends_on 'openssl' => :build
+  depends_on 'automake' => :build
+  depends_on 'autoconf' => :build
+  depends_on 'libtool' => :build
+  depends_on 'pkg-config' => :build
+  
 
   def install
     system "./autogen.sh"
